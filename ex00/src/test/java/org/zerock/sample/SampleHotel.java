c7cdd2ee796b197436fe5e15f24ef9ff9c19d821
Sunhee <sh089286@naver.com>
2021-08-20 23:47:23 +0900

    210820

--- ex00/src/test/java/org/zerock/sample/SampleHotel.java
+++ ex00/src/test/java/org/zerock/sample/SampleHotel.java
@@ -3,16 +3,18 @@ package org.zerock.sample;
 import org.springframework.stereotype.Component;
 
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 @Component
 @ToString
 @Getter
-public class SampleHotel {
+@RequiredArgsConstructor
 
+public class SampleHotel {
+	
+	@NonNull
 	private Chef chef;
 	
-	public SampleHotel(Chef chef) {
-		this.chef = chef;
-	}
 }
