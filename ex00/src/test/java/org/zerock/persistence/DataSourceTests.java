d11f07ed9ddb7eb7df8d4efcc584b7059b094f61
Sunhee <sh089286@naver.com>
2021-08-28 18:52:22 +0900

    210828

--- ex00/src/test/java/org/zerock/persistence/DataSourceTests.java
+++ ex00/src/test/java/org/zerock/persistence/DataSourceTests.java
@@ -1,5 +1,16 @@
 package org.zerock.persistence;
 
-public class DataSourceTests {
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+//Java 설정을 사용하는 경우
+//@ContextConfiguration(classes= {RootConfig.class})
+@Log4j
+public class DataSourceTests {
+	
 }
