8a6ab7cb9c04f1b858d398d5821177273e4a8034
Sunhee <sh089286@naver.com>
2021-08-28 18:55:46 +0900

    210828

--- ex00/src/test/java/org/zerock/persistence/DataSourceTests.java
+++ ex00/src/test/java/org/zerock/persistence/DataSourceTests.java
@@ -1,7 +1,12 @@
 package org.zerock.persistence;
 
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+
 import javax.sql.DataSource;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -20,5 +25,16 @@ public class DataSourceTests {
 	@Setter(onMethod_ = { @Autowired })
 	private DataSource dataSource;
 	
+	@Test
+	public void testConnection() {
+		
+		try (Connection con = dataSource.getConnection()){
+			
+			log.info(con);
+			
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
 	
 }
