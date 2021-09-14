892fa078ac60ef625cd4eb979cf39e0f51074f61
Sunhee <sh089286@naver.com>
2021-09-14 15:54:31 +0900

    210914

--- ex02/src/test/java/org/zerock/persistence/BoardMapperTests.java
+++ ex02/src/test/java/org/zerock/persistence/BoardMapperTests.java
@@ -59,8 +59,23 @@ public class BoardMapperTests {
 		 log.info(board);
 	}
 	
+	@Test
 	public void testDelete() {
 		
 		log.info("DELETE COUNT : " + mapper.delete(3L));
+	}
+	
+	@Test
+	public void testUpdate() {
+		
+		BoardVO board = new BoardVO();
+		//실행전 존재하는 번호이지 확인할 것
+		board.setBno(5L);
+		board.setTitle("수정된 제목");
+		board.setContent("수정된 내용");
+		board.setWriter("user00");
+		
+		int count = mapper.update(board);
+		log.info("UPDATE COUNT : " + count);
 	}
 }
