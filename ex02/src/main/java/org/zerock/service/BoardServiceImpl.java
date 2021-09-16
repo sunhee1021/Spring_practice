368973ddcb37df99c065954c01f06b1bbaad5552
Sunhee <sh089286@naver.com>
2021-09-16 15:59:14 +0900

    210916

--- ex02/src/main/java/org/zerock/service/BoardServiceImpl.java
+++ ex02/src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -44,7 +44,9 @@ public class BoardServiceImpl implements BoardService {
 
 	@Override
 	public List<BoardVO> getList() {
-		// TODO Auto-generated method stub
-		return null;
+
+		log.info("getList...........");
+		
+		return mapper.getList();
 	}
 }
