af7b3f68cacf17a320f7d767dbf0944ea02bfc93
Sunhee <sh089286@naver.com>
2021-09-16 16:00:06 +0900

    210916

--- ex02/src/test/java/org/zerock/service/BoardServiceTests.java
+++ ex02/src/test/java/org/zerock/service/BoardServiceTests.java
@@ -40,4 +40,11 @@ public class BoardServiceTests {
 		log.info("생성된 게시물의 번호 : " + board.getBno());
 	}
 	
+	@Test
+	public void testGetList() {
+		
+		service.getList().forEach(board -> log.info(board));
+	
+	}
+	
 }
