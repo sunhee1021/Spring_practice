59741d9b2c916fde66c02756090b296232c090d3
Sunhee <sh089286@naver.com>
2021-09-17 12:18:17 +0900

    210917

--- ex02/src/main/java/org/zerock/controller/BoardController.java
+++ ex02/src/main/java/org/zerock/controller/BoardController.java
@@ -1,10 +1,13 @@
 package org.zerock.controller;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.BoardVO;
 import org.zerock.service.BoardService;
@@ -41,5 +44,11 @@ public class BoardController {
 		//등록작업이 끝난후 다시 목록화면으로 이동하기 위함
 		//추가적으로 새롭게 등록된 게시물의 번호를 같이 전달하기 위해
 		//redirectAttributes를 이용
+	}
+	
+	public void get(@RequestParam("bno") Long bno, Model model) {
+		
+		log.info("/get");
+		model.addAttribute("board", service.get(bno));
 	}
 }
