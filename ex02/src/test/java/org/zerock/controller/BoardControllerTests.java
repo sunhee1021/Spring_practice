de0c53381ecff19bfb231fb1b92bffedf69e9f1c
Sunhee <sh089286@naver.com>
2021-09-17 11:59:26 +0900

    210917

--- ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
+++ ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -1,5 +1,54 @@
 package org.zerock.controller;
 
-public class BoardControllerTests {
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+//tomcat(was)를 실행하지 않고도 스프링과 웹 url 테스트 가능
+@RunWith(SpringJUnit4ClassRunner.class)
+
+//컨트롤러 테스트를 위한 어노테이션
+@WebAppConfiguration
+
+@ContextConfiguration({
+	"file:src/main/webapp/WEB-INF/spring/root-context.xml",
+	"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"
+})
+@Log4j
+public class BoardControllerTests {
+	
+	@Setter(onMethod_ = {@Autowired})
+	private WebApplicationContext ctx;
+	
+	private MockMvc mocMvc;
+	//가짜 mvc라고 생각하면 됨
+	//가짜로 url과 파라미터등을 브라우저에서 사용하는것처럼 만들어서
+	//controller를 실행해 볼수 있음
+	//get방식의 호출
+	
+	@Before
+	public void setup() {
+		this.mocMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+	}
+	
+	@Test
+	public void testList() throws Exception{
+		
+		log.info(
+				mocMvc.perform(MockMvcRequestBuilders.get("/board/list"))
+				.andReturn()
+				.getModelAndView()
+				.getModelMap());
+	}
 }
