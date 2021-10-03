574adab1352cc99b89380eea1d9698ee55891029
Sunhee <sh089286@naver.com>
2021-10-03 22:30:47 +0900

    211003

--- ex03/src/main/java/org/zerock/controller/SampleController.java
+++ ex03/src/main/java/org/zerock/controller/SampleController.java
@@ -1,9 +1,15 @@
 package org.zerock.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.SampleVO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -20,4 +26,39 @@ public class SampleController {
 		
 		return "안녕하세요";
 	}
+	
+	@RequestMapping(value="/getSample", method=RequestMethod.GET,
+			produces = {
+				MediaType.APPLICATION_JSON_UTF8_VALUE,
+				MediaType.APPLICATION_XML_VALUE
+			})
+	
+	public SampleVO getSample() {
+		
+		return new SampleVO(112,"스타","로드");
+	}
+	
+	@RequestMapping(value="/getSample2", method=RequestMethod.GET)
+	public SampleVO getSample2() {
+		
+		return new SampleVO(113,"스타","로드");
+	}
+	
+//	@RequestMapping(value="/getList", method=RequestMethod.GET)
+//	public List<SampleVO> getList() {
+//		
+//		return IntStream range(1,10).mapToObj(i -> 
+//		new SampleVO(i, i+"First", i+"Last"))
+//				.collect(Collectors.toList());
+//	}
+	
+	@RequestMapping(value="/getMap", method=RequestMethod.GET)
+	public Map<String, SampleVO> getMap() {
+		
+		Map<String, SampleVO> map = new HashMap<>();
+		map.put("First", new SampleVO(111,"그루트","주니어"));
+		
+		return map;
+	}
+	//373페이지부터
 }
